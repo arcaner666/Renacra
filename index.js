375f9660ba5b24fb3d6934857c1dce661e11683d
arcaner666 <arcaner666@gmail.com>
2021-07-19 09:40:54 +0300

    updated

--- index.js
+++
@@ -1,8 +0,0 @@
-// var input = document.querySelector('input');
-// var paragraph = document.querySelector('p');
-
-// input.addEventListener('click', buttonClicked);
-
-// function buttonClicked() {
-//     paragraph.textContent = "The machine started.";
-// }
