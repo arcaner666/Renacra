7cc856fe404b0ab9d27df4b05b4c1c0ac4b0b730
Arcan Caner Şenol <arcaner666@gmail.com>
2021-09-07 17:19:42 +0300

    updated

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-var {user: x} = {user: 5};
-console.log(x);
+function CreateSuspectObjects(name) {
+    return {
+        name: name,
+        color: name.split(' ')[1],
+        speak(){
+            console.log("My name is ", name);
+        }
+    };
+};
+var suspects = ["Miss Scarlet", "Colonel Mustard", "Mr. White"];
+var suspectsList = [];
+suspects.forEach(element => {
+    suspectsList.push(CreateSuspectObjects(element));
+});
+suspectsList[0].speak();
+suspectsList[1].speak();
+suspectsList[2].speak();
