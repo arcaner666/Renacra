e95aa317b7e4fad5d97b36c07f73fb2bf55e300c
Arcan Caner Şenol <arcaner666@gmail.com>
2021-09-08 17:35:57 +0300

    updated

--- index.js
+++ index.js
@@ -1,17 +1,110 @@
-function CreateSuspectObjects(name) {
-    return {
-        name: name,
-        color: name.split(' ')[1],
-        speak(){
-            console.log("My name is ", name);
-        }
-    };
+const videoData = [
+  {
+    name: "Miss Scarlet",
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+  {
+    name: "Mrs. White",
+    present: false,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+  {
+    name: "Reverend Green",
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+  {
+    name: "Rusty",
+    present: false,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+  {
+    name: "Colonel Mustard",
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+  {
+    name: "Professor Plum",
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+];
+
+const _ = {};
+
+_.filter = function (list, callback) {
+  let newList = [];
+  for (let i = 0; i < list.length; i++) {
+    if (callback(list[i], i, list) === true) {
+      newList.push(list[i]);
+    }
+  }
+  return newList;
 };
-var suspects = ["Miss Scarlet", "Colonel Mustard", "Mr. White"];
-var suspectsList = [];
-suspects.forEach(element => {
-    suspectsList.push(CreateSuspectObjects(element));
+
+_.map = function (list, callback) {
+  let newList = [];
+  for (let i = 0; i < list.length; i++) {
+    newList.push(callback(list[i], i, list));
+  }
+  return newList;
+};
+
+const findSuspects = function (value, i, list) {
+  return value.present === true;
+};
+
+const createNewArray = function (value, i, list) {
+  return value.name;
+};
+
+const suspects = _.filter(videoData, (suspectObject) => {
+  return suspectObject.present;
 });
-suspectsList[0].speak();
-suspectsList[1].speak();
-suspectsList[2].speak();
+const suspectsNames = _.map(suspects, (suspect) => {
+  return suspect.name;
+});
+console.log(suspectsNames);
