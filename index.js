be5d119c70d247d2f8dfdf5fb1f5c849e9db4426
Arcan Caner Şenol <arcaner666@gmail.com>
2021-09-14 17:36:00 +0300

    updated

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-function createFunction() {
-  function multiplyBy2(num) {
-    return num * 2;
-  }
-  return multiplyBy2;
+function printHello() {
+  console.log("Print Hello!");
 }
-const generatedFunction = createFunction();
-const result = generatedFunction(3);
-console.log(result);
+function blockForOneSecond() {
+  console.log("blockForOneSecond");
+  console.log("blockForOneSecond");
+  console.log("blockForOneSecond");
+  console.log("blockForOneSecond");
+}
+setTimeout(printHello, 0);
+blockForOneSecond();
+console.log("Me First");
