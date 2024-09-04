5d4798883c52309dbc40cfcf0e7509451abb1bb1
arcaner666 <arcaner666@gmail.com>
2024-09-04 20:27:23 +0300

    updated

--- computer-science/programming/composing-programs/examples/nested_definitions.py
+++ computer-science/programming/composing-programs/examples/nested_definitions.py
@@ -1,3 +1,21 @@
+def improve(update, close, guess=1):
+    while not close(guess):
+        guess = update(guess)
+    return guess
+
+
+def golden_update(guess):
+    return 1 / guess + 1
+
+
+def square_close_to_successor(guess):
+    return approx_eq(guess * guess, guess + 1)
+
+
+def approx_eq(x, y, tolerance=1e-15):
+    return abs((x - y) < tolerance)
+
+
 def average(x, y):
     return (x + y) / 2
 
@@ -6,4 +24,6 @@ def sqrt_update(x, a):
     return average(x, a / x)
 
 
-print()
+result = improve(golden_update, square_close_to_successor)
+
+print(result)
